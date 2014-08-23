b183869d4ee8e84aaac324bbe8c07c051570674a
Jack Danger Canty <jackdanger@squareup.com>
2014-08-23 13:16:20 -0700

    Rubocop clean.

--- lib/unix.rb
+++ lib/unix.rb
@@ -1,5 +1,7 @@
-require "unix/version"
+require 'unix/version'
 
+# Provide access to system calls on Unix by wrapping the glibc primitives in
+# Ruby objects.
 module Unix
   # Your code goes here...
 end
--- lib/unix/version.rb
+++ lib/unix/version.rb
@@ -1,3 +1,4 @@
+# rubocop:disable Style/Documentation
 module Unix
-  VERSION = "0.0.1"
+  VERSION = '0.0.1'
 end
